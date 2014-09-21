e77253c2f886cef95eb2b4053ad188de46b752f8
Shantanu <shantanuraj@ovi.com>
2014-09-21 16:29:33 +0530

    Basic API setup, respond to GET with message.

--- app/app.js
+++ app/app.js
@@ -1,12 +1,23 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
-app.get('/foo', function(req, res){
-  res.send('Hello World');
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;        // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:3000/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'Slingshot API' });   
 });
 
-app.get('/bar', function(req, res){
-  res.send('Learning ExpressJS');
-});
+app.use('/api', router);
 
 module.exports = app;
