555e12dca36649777a354b4401eeb7ca8e7a3408
Shantanu <shantanuraj@ovi.com>
2014-09-24 12:36:45 +0530

    Add nodemon as dependency, use router.route uniformly accross all URLs, get/post requests for api/bags fetches and saves bag information to/from Parse.

--- app/app.js
+++ app/app.js
@@ -4,7 +4,8 @@ var bodyParser = require('body-parser');
 
 //Parse setup
 GLOBAL.Parse = require('parse').Parse;
-Parse.initialize("W42PEkh5M824lpvx1FZsJxWBP6Eq7o9rvNHyJFP5", "5OA1f5vv9VbcCCT1reXAckJYPylUszIcRhDWUfT7");
+Parse.initialize("KCMmkNOL6FldFtccbPRvXePY56qPwZtJIyHX83Cd", "oIyRZvtLFL648BxMX3PIYxVlVP94I29WG1IK4b03");
+var Bags = Parse.Object.extend("bags");
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -15,16 +16,50 @@ app.use(bodyParser.json());
 // get an instance of the express Router
 var router = express.Router();
 
-router.get('/', function(req, res) {
-    res.json({ message: 'Slingshot API' });
-});
+function addBagToInventory(name, price, stock) {
+	var bag = new Bags();
+	bag.set("name", name);
+	bag.set("price", parseInt(price));
+	bag.set("stock", parseInt(stock));
+	bag.save(null, {
+	  success: function(bag) {
+	    console.log('New object created with objectId: ' + bag.id);
+	  },
+	  error: function(bag, error) {
+	    console.log('Failed to create new object, with error code: ' + error.message);
+	  }
+	});
+}
+
+function getInventory(res) {
+	var query = new Parse.Query(Bags);
+	query.greaterThan("stock", 0);
+
+	query.find({
+		success: function(bags) {
+			res.json(bags);
+		},
+		error: function(error) {
+			res.json( {error: "Out of stock!"} );
+		}
+	});
+}
+
+router.route('/')
+	.get(function(req, res) {
+    	res.json({ message: 'Slingshop API' });
+    });
 
 router.route('/bags')
 	.get(function(req, res) {
-		res.send('GET request for: bags');
+		getInventory(res);
 	})
 	.post(function(req, res) {
-		res.send('POST request for: bags');
+		var name  = req.body.name,
+			price = req.body.price;
+			stock = req.body.stock;
+		res.json({ message: 'Adding to inventory', name: name, price: price, stock: stock });
+		addBagToInventory(name, price, stock);
 	});
 
 app.use('/api', router);
