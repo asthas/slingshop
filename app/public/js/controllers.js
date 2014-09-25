520ed9a6bc92a4f2fe7a46bce9f8e085a302f57a
Shantanu <shantanuraj@ovi.com>
2014-09-25 13:19:40 +0530

    Display bag info using Angular, load photos from Parse (hacky solution), removed local copies of photos.

--- app/public/js/controllers.js
+++ app/public/js/controllers.js
@@ -2,11 +2,9 @@ var controllers = angular.module('slingcontrol', []);
 
 controllers.controller('MainCtrl', function($scope, $http){
 	$scope.loading = true;
-	$scope.bagsCount = 'Loading...';
 	var url ='http://localhost:3000/api/bags?callback=JSON_CALLBACK';
  	$http.jsonp(url).then(function(response){
  		$scope.loading = false;
  		$scope.bagsList = response.data;
- 		$scope.bagsCount = 'Bags in stock: ' + response.data.length;
  	});
 });
